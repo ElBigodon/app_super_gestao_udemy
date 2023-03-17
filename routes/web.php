37eedbe07f65b52ecb0b64771685201027f75211
ElBigodon <alexandremuassab@gmail.com>
2023-03-17 10:02:45 -0300

    fazendo uma rota simples com paramêtros

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,22 @@ Route::get('/', 'App\Http\Controllers\MainController@main');
 Route::get('/sobre', 'App\Http\Controllers\AboutController@about');
 
 Route::get('/contato', 'App\Http\Controllers\ContactController@contact');
+
+/**
+ * Essa rota terá 4 parametros, sendo elas:
+ * [name, category, subject, message]
+ *
+ * Caso seja especificado o parametro na rota, o mesmo será inserido na função
+ *
+ *
+ *           Rota ↓              ↓ Parametros ↓                ↓ Função (Callback)
+ */
+Route::get('/contato/{name}/{category}/{subject}/{message}', function (
+  string $name,
+  string $category,
+  string $subject,
+  string $message
+)
+{
+  return "Olá $name, sobre o seu assunto: '$subject', com a sua mensagem: '$message' estaremos passando para o setor $category";
+});
