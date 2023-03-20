4edd0dc0c20f689ed2a99e42de28b51e095db511
ElBigodon <alexandremuassab@gmail.com>
2023-03-20 20:10:25 -0300

    testando o if elseif e else no blade

--- resources/views/app/provider/main.blade.php
+++ resources/views/app/provider/main.blade.php
@@ -2,8 +2,8 @@
     Fornecedor
 </h3>
 
-{{ 'Podemos usar essa forma para aplicar um código dentro do blade, como:' }}
-<?= 'Como dessa forma.' ?>
+{{-- {{ 'Podemos usar essa forma para aplicar um código dentro do blade, como:' }} --}}
+{{-- <?= 'Como dessa forma.' ?> --}}
 
 {{-- Dessa forma é feito o comentário dentro do blade. --}}
 
@@ -13,10 +13,22 @@
     // Comentário de uma linha
 
     /*
-  Comentário de várias linhas
-  foo bar
-*/
+        Comentário de várias linhas
+        foo bar
+    */
 
-    echo "Texto usando o 'echo' do php puro";
-
+    // echo "Texto usando o 'echo' do php puro";
 @endphp
+
+{{-- Executando e visualizando os conteúdos da váriavel --}}
+{{-- @dd($providers) --}}
+
+{{-- Fazendo if, else if, else na syntax do blade --}}
+@if (count($providers) > 0 && count($providers) < 10)
+  há de 0 a 10 fornecedores cadastrados
+@elseif (count($providers) > 9)
+  há mais de 10 fornecedores cadastrados
+@else
+  não há fornecedores cadastrados
+@endif
+
