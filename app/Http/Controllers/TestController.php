79f01704a8d3e95165f6687446962e761022345c
ElBigodon <alexandremuassab@gmail.com>
2023-03-20 10:13:15 -0300

    encaminhando parâmetros pelo view usando métodos e array associativa

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -8,6 +8,19 @@ class TestController extends Controller
 {
   public function main(int $p1, int $p2)
   {
-    echo "A soma de $p1 + $p2 é ".($p1 + $p2);
+    /**
+     * $p1 e $p2, serão transmitidos através da view, possiblitando que o mesmo use esses dois parâmetros
+     *
+     * Usando Array Associativa:
+     * return view('site/test', ['p1' => $p1, 'p2' => $p2]);
+     *
+     * Usando Compact:
+     * return view('site/test', compact('p1', 'p2'));
+     *
+     * Usando With:
+     * return view('site/test')->with('p1', $p1)->with('p2', $p2);
+     *
+     * estarei usando o método `compact()` pois eu ô achei a melhor forma de passar os paramêtros */
+    return view('site/test', compact('p1', 'p2'));
   }
 }
