99baa5c1cac9530527b6d951cf52a6e1fdc93966
ElBigodon <alexandremuassab@gmail.com>
2023-03-20 20:42:20 -0300

    testando a função @unless

--- resources/views/app/provider/main.blade.php
+++ resources/views/app/provider/main.blade.php
@@ -8,7 +8,7 @@
 {{-- Dessa forma é feito o comentário dentro do blade. --}}
 
 {{-- Caso queira implementar um código php no blade, podemos fazer dessa forma: --}}
-@php
+{{-- @php
 
     // Comentário de uma linha
 
@@ -18,17 +18,29 @@
     */
 
     // echo "Texto usando o 'echo' do php puro";
-@endphp
+@endphp --}}
 
 {{-- Executando e visualizando os conteúdos da váriavel --}}
 {{-- @dd($providers) --}}
 
 {{-- Fazendo if, else if, else na syntax do blade --}}
-@if (count($providers) > 0 && count($providers) < 10)
+{{-- @if (count($providers) > 0 && count($providers) < 10)
   há de 0 a 10 fornecedores cadastrados
 @elseif (count($providers) > 9)
   há mais de 10 fornecedores cadastrados
 @else
   não há fornecedores cadastrados
-@endif
+@endif --}}
 
+{{-- @if (condição) // executa se condição for verdadeira  --}}
+{{-- @unless inverte a condição de cima, tratando a condição se for falsa --}}
+{{--
+  Não imagino um cenário na qual o @if seja passado caso eu queira pegar o valor falso da variável
+  Ou eu passo o @if ou @unless
+ --}}
+@if($providers[0]['status'] == true)
+  ativo
+@endif
+@unless ($providers[0]['status'] == true)
+  inativo
+@endunless
